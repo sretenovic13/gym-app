714da24056fb6a6e08eb6f9509b9602ed9a99ffe
sretenovic13 <sretenovic.stefan@yahoo.com>
2024-09-04 23:48:42 +0200

    wourkout

--- gym-connect/src/components/Hero.jsx
+++ gym-connect/src/components/Hero.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Button from './Button'
 
 export default function Hero() {
   return (
-    <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[900px] w-full mx-auto p-4'>
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4'>
         <div className='flex flex-col gap-4'>
             <p>GET READY TO UNLEASH YOUR</p>
             <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>
@@ -12,9 +13,7 @@ export default function Hero() {
         <p className='text-sm md:text-base font-light'>
             By proceeding, I acknowledge that I might become <span className='text-blue-400 font-medium'>immensely powerful</span> and accept the challenge of transforming into a <span className='text-blue-400 font-medium'>fitness titan</span>. I understand that my journey may lead to unparalleled strength and a presence that demands attention.
         </p>
-        <button className='px-4 py-4 rounded-md border-[2px] border-blue-400 border-solid blueShadow bg-solid'>
-            <p>Accept & Begin</p>
-        </button>
+        <Button text={'Accept & Begin'}></Button>
     </div>
   )
 }
--- gym-connect/src/components/Workout.jsx
+++ gym-connect/src/components/Workout.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import SectionWrapper from './SectionWrapper'
+import ExerciseCard from './ExerciseCard'
 
-export default function Workout() {
+export default function Workout(props) {
+  const { workout } = props
+  console.log('Workout props:', workout)
   return (
-    <div>Workout</div>
+    <SectionWrapper header={"welcome to"} title={['The', 'DANGER', 'zone']}>
+      <div className='flex flex-col gap-4'>
+        {workout.map((exercise, i) => {
+          return (
+            <ExerciseCard
+              exercise={exercise} i={i} key={i}
+            />
+          )
+        })}
+      </div>
+    </SectionWrapper>
   )
 }
